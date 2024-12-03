1b70658d277be14c1c1d685138547cc7f4511bb7
Cedric <cedrichayfield@gmail.com>
2024-12-02 21:02:08 -0500

    Ajout des class cpp

---
+++ Source/Chess/KnightPiece.cpp
@@ -0,0 +1,2 @@
+#include "KnightPiece.h"
+
---
+++ Source/Chess/KnightPiece.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChessPiece.h"
+#include "KnightPiece.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESS_API AKnightPiece : public AChessPiece
+{
+	GENERATED_BODY()
+	
+};
